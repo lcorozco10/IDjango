c0d8c3af40178b9a40471bf7883dfa13974b3423
Luis <lcorozco10@gmail.com>
2015-07-10 17:28:21 -0600

    init commit

--- polls/views.py
+++ polls/views.py
@@ -9,8 +9,17 @@ def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-    #return JsonResponse({'l': 'sd', 'Ex': True})
+    #return HttpResponse("You're looking at question %s." % question_id)
+    return JsonResponse({
+        'firstName': 'Luis',
+        'lastName': "Orozco",
+        'Example': {
+            "Data1": True,
+            "Data2": (1, 2, 3, 4),
+            "Data3": [5, 6, 7, 8]
+        }
+    })
+
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
