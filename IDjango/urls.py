99011088807b58334079ade5f933c8378a2ddc3b
Luis <lcorozco10@gmail.com>
2015-07-16 18:13:40 -0600

    -Created  static directory and  rename app  to IDjango -unit testing example

--- IDjango/urls.py
+++ IDjango/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('polls.urls')),
     url(r'^polls/', include('polls.urls', namespace="polls")),
 ]
